42ce8eba60eef4703ba88ccd089d6f67f4d31ff5
dv250124 <dv250124@WARDV250124-L87.corp.ncr.com>
2018-07-22 21:42:16 -0300

    No changes

--- WebFrontMvn/src/main/java/abmPosOperator/SinCodigoLibre.java
+++ WebFrontMvn/src/main/java/abmPosOperator/SinCodigoLibre.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
@@ -100,12 +104,70 @@ public class SinCodigoLibre {
 		driver.get(url);
 	}
 
+	@Test (priority=3)
 	public void createWFUser() {
 		
+		//Parameters of admin user
 		String adminUser = sh.getCell(1, 5).getContents();
 		String adminPass = sh.getCell(2, 5).getContents();
 		
-		UsefulMethodsWF.loginWF(driver, adminUser, adminPass);
+		//Parameters of WebFront test user
+		String testUser = sh.getCell(1, 6).getContents();
+		String testPass = sh.getCell(2, 6).getContents();
+		String role = sh.getCell(3, 6).getContents();
+		String functionality = sh.getCell(4, 6).getContents();
+		String menuBehaviour = sh.getCell(5, 6).getContents();
+		
+		UsefulMethodsWF.createWFTestUser(adminUser, adminPass, testUser, testPass, role, functionality, menuBehaviour, driver);
+	}
+
+	@Test (priority=4)
+	public void createCashier() {
+		
+		// Login as a user
+		String user, pass;
+		user = sh.getCell(1,6).getContents();
+		pass = sh.getCell(2,6).getContents();
+		UsefulMethodsWF.loginWF(driver, user, pass);
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='verticalmenu z-div']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElements(By.xpath("//*[@class='z-toolbarbutton-cnt']")).get(0))).click();
+		
+		//Click on Insertar button
+		wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.className("z-button-cm"), 2));
+		driver.findElements(By.className("z-button-cm")).get(1).click();
+		
+		//Selecting the role of the operator
+		wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//select[@class='z-selectbox']"), 2));
+		Select select = new Select(wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@class='z-selectbox' and not (@disabled='disabled')]"))));
+		select.selectByVisibleText("Cajero");
+		
+		//Check if the error message is displayed
+		Assert.assertEquals(true, wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='z-messagebox-window z-window-highlighted z-window-highlighted-shadow']//span[@class='z-label' and text()='No hay códigos libres']"))).isDisplayed());
+		
+		//Click on OK
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(
+				"//*[@class='z-messagebox-window z-window-highlighted z-window-highlighted-shadow']//*[@class='z-messagebox-btn z-button-os']"))).click();
+		
+		//Click on Cancelar
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("z-modal-mask")));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='z-button-cm' and text()=' Cancelar']"))).click();
+		
+		//Logout of WF
+		UsefulMethodsWF.logoutWF(driver);
+	}
+
+	@Test (priority=5)
+	public void deleteWFUser() {
+		
+		//Parameters of admin user
+		String adminUser = sh.getCell(1, 5).getContents();
+		String adminPass = sh.getCell(2, 5).getContents();
+		
+		//Parameters of WebFront test user
+		String testUser = sh.getCell(1, 6).getContents();
+		
+		UsefulMethodsWF.deleteWFTestUser(adminUser, adminPass, testUser, driver);
 	}
 	
 	@Test (priority=10)
