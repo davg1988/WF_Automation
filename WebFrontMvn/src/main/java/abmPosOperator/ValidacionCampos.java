d03d25ca12042d85084ac011708715328047f43c
dv250124 <dv250124@WARDV250124-L87.corp.ncr.com>
2018-08-10 13:12:31 -0300

    Decrease the number of methods on the class, joining existing classes

--- WebFrontMvn/src/main/java/abmPosOperator/ValidacionCampos.java
+++ WebFrontMvn/src/main/java/abmPosOperator/ValidacionCampos.java
@@ -2,11 +2,8 @@ package abmPosOperator;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -32,7 +29,7 @@ public class ValidacionCampos {
 	public File fl;
 	public RServiceClientFactory factory;
 	
-	@Test (priority=1)
+	@Test (priority=1, groups = {"FullAutomated"})
 	public void launchWF() throws BiffException, IOException {
 		
 		//Extracting parameters from Excel file
@@ -52,62 +49,27 @@ public class ValidacionCampos {
 		//Setting the IP for the clients of Rest Service
 		Environment.setEnv_ip(url);
 		factory = new RServiceClientFactory();
-	}
 		
-	@Test (priority=2)
-	public void createTestUser () {
-			
-		//Getting admin credentials from excel file
+		//Getting parameters from excel file
 		String adminUser = sh.getCell(1,5).getContents();
 		String adminPass = sh.getCell(2,5).getContents();
+		String user = sh.getCell(1,6).getContents();
+		String pass = sh.getCell(2,6).getContents();
+		String role = sh.getCell(3,6).getContents();
+		String functionality = sh.getCell(4,6).getContents();
+		String menuBehaviour = sh.getCell(5,6).getContents();
 			
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@title='Introduzca el Nombre de Usuario']"))).sendKeys(adminUser);
-		driver.findElement(By.xpath("//*[@class='textlogin z-textbox' and @title='Tipee la Contraseña del Usuario']")).sendKeys(adminPass);
-		driver.findElement(By.xpath("//*[@class='z-button-cm' and text()=' Confirmar']")).click();
-			
-		//Click on Gestion Login
-		wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//*[@class='z-toolbarbutton-cnt']"),1)).get(1).click();
-			
-		//Click on Insertar button
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[@class='z-button-cm' and text()=' Insertar']"))).click();
-			
-		//Getting data of WebFront User to create for the execution of test
-		String user, pass, role, functionality, menuBehaviour;
-		user = sh.getCell(1,6).getContents();
-		pass = sh.getCell(2,6).getContents();
-		role = sh.getCell(3,6).getContents();
-		functionality = sh.getCell(4,6).getContents();
-		menuBehaviour = sh.getCell(5,6).getContents();
-
-		List<WebElement> txtFields = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//*[@class='z-textbox']"), 2));
-		txtFields.get(1).sendKeys(user);
-		txtFields.get(2).sendKeys(pass);
-		Select roleList = new Select(driver.findElements(By.xpath("//*[@class='z-selectbox']")).get(0));
-		roleList.selectByVisibleText(role);
-		Select functionalityList = new Select(driver.findElements(By.xpath("//*[@class='z-selectbox']")).get(1));
-		functionalityList.selectByVisibleText(functionality);
-		Select selectMenuBehaviour = new Select(driver.findElements(By.xpath("//*[@class='z-selectbox']")).get(2));
-		selectMenuBehaviour.selectByVisibleText(menuBehaviour);
-		driver.findElement(By.xpath("//*[@class='z-button-cm' and text()=' Confirmar']")).click();
-			
-		//Logout
-		UsefulMethodsWF.logoutWF(driver);
-	}
-	
-	@Test (priority=3)
-	public void goToMantenimientoUsuarios() {
+		UsefulMethodsWF.createWFTestUser(adminUser, adminPass, user, pass, role, functionality, menuBehaviour, driver);
 		
-		// Login as a user
-		String user, pass;
-		user = sh.getCell(1,6).getContents();
-		pass = sh.getCell(2,6).getContents();
+		// Login with the test user
 		UsefulMethodsWF.loginWF(driver, user, pass);
 		
+		//Navigate to mantenimiento de usuarios
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='verticalmenu z-div']"))).click();
 		wait.until(ExpectedConditions.elementToBeClickable(driver.findElements(By.xpath("//*[@class='z-toolbarbutton-cnt']")).get(0))).click();
 	}
 	
-	@Test (priority=4)
+	@Test (priority=4, groups = {"FullAutomated"})
 	public void validateFields() throws Exception {
 
 		int count = 1;
@@ -166,43 +128,15 @@ public class ValidacionCampos {
 		UsefulMethodsWF.logoutWF(driver);
 	}
 	
-	@Test (priority=9)
+	@Test (priority=9, groups = {"FullAutomated"})
 	public void deleteTestUser() {
 		
 		//Login as an admin
 		String user, pass;
 		user = sh.getCell(1,5).getContents();
 		pass = sh.getCell(2,5).getContents();
-		UsefulMethodsWF.loginWF(driver, user, pass);
-		
-		//Go To Gestion Login
-		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//*[@class='z-window-embedded']//*[@class='z-toolbarbutton-cnt']"))).get(1).click();
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("z-modal-mask")));
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='titlebar nomargin_left z-hbox']"))).click();
-		
-		//Find test user
-		List<WebElement> white_rows = driver.findElements(By.xpath("//*[@class='z-row']//*[@class='z-listcell-cnt z-overflow-hidden']"));
-		List<WebElement> gray_rows = driver.findElements(By.xpath("//*[@class='z-row z-grid-odd']//*[@class='z-listcell-cnt z-overflow-hidden']"));
 		String name = sh.getCell(1,6).getContents();
-		int located_index = 0;
-		int i = 0;
-		while(i<white_rows.size()) {
-			if(white_rows.get(i).getText().equals(name)) {
-				located_index = i*2;
-				i = white_rows.size();
-			}				
-		}
-		int j = 0;
-		while(j<gray_rows.size()) {
-			if(gray_rows.get(i).getText().equals(name)) {
-				located_index = j+j+1;
-				j = gray_rows.size();
-			}
-		}
-		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//*[@class='z-button-cm' and text()=' Eliminar']"))).get(located_index).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='z-window-highlighted-cnt']//*[@class='z-messagebox-btn z-button-os' and text()='Yes']"))).click();
-		UsefulMethodsWF.logoutWF(driver);
-		driver.close();
+		UsefulMethodsWF.deleteWFTestUser(user, pass, name, driver);
 	}
 
 }
