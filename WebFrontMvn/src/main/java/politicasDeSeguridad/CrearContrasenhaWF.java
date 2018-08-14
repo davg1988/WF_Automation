92b8694a203b6a54711e48b5392205270f0ac44c
dv250124 <dv250124@WARDV250124-L87.corp.ncr.com>
2018-08-14 17:58:07 -0300

    Include method to set up WebDriver driver

--- WebFrontMvn/src/main/java/politicasDeSeguridad/CrearContrasenhaWF.java
+++ WebFrontMvn/src/main/java/politicasDeSeguridad/CrearContrasenhaWF.java
@@ -7,8 +7,6 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,7 +15,6 @@ import org.testng.annotations.Test;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
-import utilidades.ScreenShot;
 import utilidades.UsefulMethodsWF;
 
 public class CrearContrasenhaWF {
@@ -30,48 +27,34 @@ public class CrearContrasenhaWF {
 	
 	@Test (priority=1)
 	public void launchWF() throws BiffException, IOException {
-		fl = new File("Parametros\\PoliticasDeSeguridad\\politicas.xls");
-		wb = Workbook.getWorkbook(fl);
-		sh = wb.getSheet("CrearContrasenhaWF");
-		String url = sh.getCell(1,2).getContents();
-		System.setProperty("webdriver.chrome.driver", "ChromeDriver\\chromedriver.exe");
-		ChromeOptions op = new ChromeOptions();
-		op.addArguments("--start-maximized");
-		driver = new ChromeDriver(op);
-		wait = new WebDriverWait(driver, 30);
-		driver.get(url);
-	}
-	
-	@Test (priority=2)
-	public void loginAdmin() {
 		
-		//Getting admin credentials from excel file
-		String adminUser = sh.getCell(1,4).getContents();
-		String adminPass = sh.getCell(2,4).getContents();
-		
-		UsefulMethodsWF.loginWF(driver, adminUser, adminPass);
-	}	
-	
-	@Test (priority=3)
-	public void navigation() {
-		
-		//Click on Gestion Login
-		wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//*[@class='z-toolbarbutton-cnt']"),1)).get(1).click();
-		
-		//Click on Insertar button
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[@class='z-button-cm' and text()=' Insertar']"))).click();
+		driver = UsefulMethodsWF.setUpWf(); 
+		wait = new WebDriverWait(driver, 45);
 	}
 	
 	@Test (priority=4)
 	public void PositiveCase() throws Exception {
 		
+		// Login as an admin
+		UsefulMethodsWF.loginAdmin(driver);
+		
+		// Click on Gestion Login
+		wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//*[@class='z-toolbarbutton-cnt']"),1)).get(1).click();
+		
+		// Click on Insertar button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[@class='z-button-cm' and text()=' Insertar']"))).click();
+		
+		// Setting data provider
+		fl = new File("Parametros\\PoliticasDeSeguridad\\politicas.xls");
+		wb = Workbook.getWorkbook(fl);
+		sh = wb.getSheet("CrearContrasenhaWF");
+		
 		//Getting parameters from the excel file
-		String user, pass, role, functionality, testCase;
+		String user, pass, role, functionality;
 		user = sh.getCell(1,7).getContents();
 		pass = sh.getCell(2,7).getContents();
 		role = sh.getCell(3, 7).getContents();
 		functionality = sh.getCell(4, 7).getContents();
-		testCase = sh.getCell(5, 7).getContents();
 		
 		List<WebElement> txtFields = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//*[@class='z-textbox']"), 2));
 		txtFields.get(1).sendKeys(user);
@@ -81,8 +64,6 @@ public class CrearContrasenhaWF {
 		Select functionalityList = new Select(driver.findElements(By.xpath("//*[@class='z-selectbox']")).get(1));
 		functionalityList.selectByVisibleText(functionality);
 		driver.findElement(By.xpath("//*[@class='z-button-cm' and text()=' Confirmar']")).click();
-		
-		ScreenShot.takeSnapShot(driver, "Evidencia\\PoliticasDeSeguridad\\"+testCase+".png");
 		
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[@class='z-button-cm' and text()=' Insertar']")));
 		int btnSize = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//*[@class='z-button-cm' and text()=' Eliminar']"), 1)).size();
@@ -108,10 +89,11 @@ public class CrearContrasenhaWF {
 		for (int i = 8; i < 13; i++) {
 			
 			//Getting data from excel file
-			String user, pass, testCase;
+			String user, pass;
 			user = sh.getCell(1,i).getContents();
-			pass = sh.getCell(2,i).getContents();	
-			testCase = sh.getCell(5, i).getContents();
+			pass = sh.getCell(2,i).getContents();
+			String role = sh.getCell(3, i).getContents();
+			String functionality = sh.getCell(4, i).getContents();
 			
 			List<WebElement> fields = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//*[@class='z-textbox']"), 1));
 			driver.findElements(By.xpath("//*[@class='z-textbox']")).clear();
@@ -123,18 +105,18 @@ public class CrearContrasenhaWF {
 			}
 			driver.findElement(By.xpath("//*[@class='z-textbox' and @type='password']")).sendKeys(pass);
 			Select listRole = new Select(driver.findElements(By.xpath("//*[@class='z-selectbox']")).get(0));
-			listRole.selectByVisibleText("User");
+			listRole.selectByVisibleText(role);
 			Select listFunctionality = new Select(driver.findElements(By.xpath("//*[@class='z-selectbox']")).get(1));
-			listFunctionality.selectByVisibleText("General");
+			listFunctionality.selectByVisibleText(functionality);
 		
 			//Confirm the input data
 			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@class='z-modal-mask']")));
 			driver.findElement(By.xpath("//*[@class='z-button-cm' and text()=' Confirmar']")).click();
-			ScreenShot.takeSnapShot(driver, "Evidencia\\PoliticasDeSeguridad\\"+testCase+".png");
 			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='z-window-highlighted-cnt']//*[@class='z-messagebox-btn z-button-os']"))).click();
 
 		} // End of the loop for negative test cases
 		
 		UsefulMethodsWF.logoutWF(driver);
+		driver.close();
 	}
 }
