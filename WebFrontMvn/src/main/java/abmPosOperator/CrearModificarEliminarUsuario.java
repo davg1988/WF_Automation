62d9a4ad90731f356968e73aa58d25e36ff119de
dv250124 <dv250124@WARDV250124-L87.corp.ncr.com>
2018-08-15 09:42:22 -0300

    Update
    change the locator of the login field on teh create pos operator method

--- WebFrontMvn/src/main/java/abmPosOperator/CrearModificarEliminarUsuario.java
+++ WebFrontMvn/src/main/java/abmPosOperator/CrearModificarEliminarUsuario.java
@@ -17,8 +17,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import jxl.Sheet;
@@ -177,8 +175,8 @@ public class CrearModificarEliminarUsuario {
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='z-textbox' and @maxlength='20']"))).sendKeys(name);
 		
 		// Filling the login field
-		driver.findElement(By.xpath("//*[@class='z-longbox']")).clear();
-		driver.findElement(By.xpath("//*[@class='z-longbox z-longbox-focus']")).sendKeys(login);
+		driver.findElements(By.xpath("//*[@class='z-textbox']")).get(3).clear();
+		driver.findElement(By.xpath("//*[@class='z-textbox z-textbox-focus']")).sendKeys(login);
 		
 		// Click on Confirmar button
 		driver.findElement(By.xpath("//*[@class='z-button-cm' and text()=' Confirmar']")).click();
