1184f9026917a3289fa7762129efea897159a9a2
Viloria <dv185079@ncr.com>
2020-03-03 12:07:00 -0300

    Change the scope of testNG in pom.xml file.
    Change Assert used on SinCodigoLibre and ValidacionCampos

--- WebFrontMvn/src/main/java/abmPosOperator/ValidacionCampos.java
+++ WebFrontMvn/src/main/java/abmPosOperator/ValidacionCampos.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import junit.framework.Assert;
 import jxl.Sheet;
 import jxl.Workbook;
 import utilidades.UsefulMethodsWF;
